57ebc5b24bd493828f37f6c71727c696a8e9ec46
hsdehaini <99140276+hsdehaini@users.noreply.github.com>
2023-10-02 17:46:24 -0700

    Making cmake work

--- src/hw1.cpp
+++ src/hw1.cpp
@@ -30,6 +30,7 @@ Image3 hw_1_1(const std::vector<std::string> &params) {
     for (int y = 0; y < img.height; y++) {
         for (int x = 0; x < img.width; x++) {
             img(x, y) = Vector3{1, 1, 1};
+            
         }
     }
     return img;
