038106696caacaeb8f79be4069a62e704b4a6ad9
hsdehaini <99140276+hsdehaini@users.noreply.github.com>
2023-10-28 23:20:02 -0700

    2.1 finished

--- src/hw2.cpp
+++ src/hw2.cpp
@@ -5,8 +5,11 @@ using namespace hw2;
 
 Image3 hw_2_1(const std::vector<std::string> &params) {
     // Homework 2.1: render a single 3D triangle
+    
+    int img_width = 640;
+    int img_height = 480;
 
-    Image3 img(640 /* width */, 480 /* height */);
+    Image3 img(img_width, img_height);
 
     Vector3 p0{0, 0, -1};
     Vector3 p1{1, 0, -1};
@@ -39,9 +42,50 @@ Image3 hw_2_1(const std::vector<std::string> &params) {
         }
     }
 
+    Vector2 h0 = Vector2(p0.x / -p0.z, p0.y / -p0.z);
+    Vector2 h1 = Vector2(p1.x / -p1.z, p1.y / -p1.z);
+    Vector2 h2 = Vector2(p2.x / -p2.z, p2.y / -p2.z);
+
+    Real a = img_width / img_height;
+
+    Vector2 t0 = Vector2((img_width) * ((h0.x + (s * a)) / (2 * s * a)), (img_height) * ((-h0.y + (s * a)) / (2 * s * a)));
+    Vector2 t1 = Vector2((img_width) * ((h1.x + (s * a)) / (2 * s * a)), (img_height) * ((-h1.y + (s * a)) / (2 * s * a)));
+    Vector2 t2 = Vector2((img_width) * ((h2.x + (s * a)) / (2 * s * a)), (img_height) * ((-h2.y + (s * a)) / (2 * s * a)));
+
     for (int y = 0; y < img.height; y++) {
         for (int x = 0; x < img.width; x++) {
-            img(x, y) = Vector3{1, 1, 1};
+            Vector3 pixelColor = color;
+            Vector3 subpixelColor = color;
+            for (int j = 0; j < 4; j++) {
+                for (int k = 0; k < 4; k++) {
+                    Real subpixelX = (x + (j + 0.5) / 4.0);
+                    Real subpixelY = (y + (k + 0.5) / 4.0);
+                    Vector2 subpixelCenter(subpixelX, subpixelY);
+
+                    if(p0.z < -z_near && p1.z < -z_near && p2.z < -z_near) {
+                        Vector2 e01 = t1 - t0;
+                        Vector2 e12 = t2 - t1;
+                        Vector2 e20 = t0 - t2;
+
+                        Vector2 n01(e01.y, -e01.x);
+                        Vector2 n12(e12.y, -e12.x);
+                        Vector2 n20(e20.y, -e20.x);
+
+                        Vector2 v0 = t0 - subpixelCenter;
+                        Vector2 v1 = t1 - subpixelCenter;
+                        Vector2 v2 = t2 - subpixelCenter;
+                        
+                        if ((dot(v0, n01) >= 0) && (dot(v1, n12) >= 0) && (dot(v2, n20) >= 0) || (dot(v0, n01) <= 0) && (dot(v1, n12) <= 0) && (dot(v2, n20) <= 0)) {
+                            subpixelColor = color;
+                        } else {
+                            subpixelColor = Vector3{0.5, 0.5, 0.5};
+                        }
+                    }
+                    pixelColor += subpixelColor;
+                }
+            }
+            pixelColor /= Real(16);
+            img(x, y) = pixelColor;
         }
     }
     return img;
