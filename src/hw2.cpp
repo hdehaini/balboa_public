650da6d963d3b5ab7c3e119a45c3ff15beda885e
hsdehaini <99140276+hsdehaini@users.noreply.github.com>
2023-10-30 13:04:10 -0700

    Fixing 2.2

--- src/hw2.cpp
+++ src/hw2.cpp
@@ -95,8 +95,13 @@ Image3 hw_2_2(const std::vector<std::string> &params) {
     // Homework 2.2: render a triangle mesh
 
     Image3 img(640 /* width */, 480 /* height */);
+    int img_width = 640;
+    int img_height = 480;
+
+    Vector3 background = Vector3{0.5, 0.5, 0.5};
 
     Real s = 1; // scaling factor of the view frustrum
+    Real a = img.width / img.height;
     Real z_near = 1e-6; // distance of the near clipping plane
     int scene_id = 0;
     for (int i = 0; i < (int)params.size(); i++) {
@@ -110,11 +115,93 @@ Image3 hw_2_2(const std::vector<std::string> &params) {
     }
 
     TriangleMesh mesh = meshes[scene_id];
-    UNUSED(mesh); // silence warning, feel free to remove this
 
+    // UNUSED(mesh); // silence warning, feel free to remove this
+
+    
+    
     for (int y = 0; y < img.height; y++) {
         for (int x = 0; x < img.width; x++) {
-            img(x, y) = Vector3{1, 1, 1};
+            Vector3 pixelColor = background;
+            Vector3 subpixelColor = background;
+            for (int j = 0; j < 4; j++) {
+                for (int k = 0; k < 4; k++) {
+                    Real z_min = 1e6;
+                    Real subpixelX = (x + (j + 0.5) / 4.0);
+                    Real subpixelY = (y + (k + 0.5) / 4.0);
+                    Vector2 subpixelCenter(subpixelX, subpixelY);
+
+                    for (auto face : mesh.faces) {
+                        
+                        Vector3 p0 = mesh.vertices[face.x];
+                        Vector3 p1 = mesh.vertices[face.y];
+                        Vector3 p2 = mesh.vertices[face.z];
+
+                        if(p0.z < -z_near && p1.z < -z_near && p2.z < -z_near) {
+
+
+                            // Getting p', p0', p1' and p2'
+                            Vector3 p_prime = Vector3(subpixelCenter.x + 0.5, subpixelCenter.y + 0.5, Real(-1));
+                            Vector3 p0_prime = Vector3(p0.x / -p0.z, p0.y / -p0.z, Real(-1));
+                            Vector3 p1_prime = Vector3(p1.x / -p1.z, p1.y / -p1.z, Real(-1));
+                            Vector3 p2_prime = Vector3(p2.x / -p2.z, p2.y / -p2.z, Real(-1));
+
+                            // Finding edge vectors
+                            Vector3 e1 = p1_prime - p0_prime;
+                            Vector3 e2 = p2_prime - p0_prime;
+
+                            Real denominator = length(cross(e1, e2)) / 2;
+
+
+                            Vector3 e3 = p1_prime - p_prime;
+                            Vector3 e4 = p2_prime - p_prime;
+
+                            Real b0_num = length(cross(e3, e4)) / 2;
+
+
+                            Vector3 e5 = p_prime - p0_prime;
+                            Vector3 e6 = p2_prime - p0_prime;
+
+                            Real b1_num = length(cross(e5, e6)) / 2;
+
+
+                            Vector3 e7 = p1_prime - p0_prime;
+                            Vector3 e8 = p_prime - p0_prime;
+                            
+                            Real b2_num = length(cross(e7, e8)) / 2;
+
+
+                            // getting barycentric prime coordinates
+                            Real b0_prime = b0_num / denominator;
+                            Real b1_prime = b1_num / denominator;
+                            Real b2_prime = b2_num / denominator;
+
+
+                            // getting barycentric coordinates
+                            Real b_den = (b0_prime / p0.z) + (b1_prime / p1.z) + (b2_prime / p2.z);
+
+                            Real b0 = (b0_prime / p0.z) / b_den;
+                            Real b1 = (b1_prime / p1.z) / b_den;
+                            Real b2 = (b2_prime / p2.z) / b_den;
+
+
+                            // getting depth
+                            Real depth = (b0 * p0.z) + (b1 * p1.z) + (b2 * p2.z);
+
+                            if (depth < z_min) {
+                                subpixelColor = mesh.face_colors[face.x];
+                                z_min = depth;
+                                std::cout << depth << std::endl;
+                            } else {
+                                subpixelColor = Vector3{0.5, 0.5, 0.5};
+                            }
+                        }
+                    }
+                    pixelColor += subpixelColor;
+                }
+            }
+            pixelColor /= Real(16);
+            img(x, y) = pixelColor;
         }
     }
     return img;
