be43efebd653c1e38406142ad343f6f1db3aaaa1
Hamza <hsdehaini@hotmail.com>
2023-11-13 17:20:45 -0800

    Fixing 3.3

--- src/hw3.cpp
+++ src/hw3.cpp
@@ -311,50 +311,75 @@ void hw_3_3(const std::vector<std::string> &params) {
         0.0f, 0.0f, 1.0f  // blue
     };
 
-    unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-    glBindVertexArray(VAO);
+    std::vector<GLuint> VAOs;
+    std::vector<GLuint> VBO_vertex;
+    std::vector<GLuint> VBO_color;
+    std::vector<GLuint> EBOs;
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    for(int i = 0; i < scene.meshes.size(); i++) {
+        glGenBuffers(1, &VBO_vertex[i]);
+        glGenBuffers(1, &VBO_color[i]);
+        glGenBuffers(1, &EBOs[i]);
+        glGenVertexArrays(1, &VAOs[i]);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO_vertex[i]);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(scene.meshes.ver), vertices, GL_STATIC_DRAW);
+        // Vertex Position Attribute
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
 
-    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+        glBindBuffer(GL_ARRAY_BUFFER, VBO_color[i]);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+        // Vertex Color Attribute
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(1);
 
-    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-    glBindVertexArray(0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOs[i]);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
+        glBindVertexArray(VAOs[i]);
+
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO_vertex);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+            // Vertex Position Attribute
+        glBindBuffer(GL_ARRAY_BUFFER, VBO_vertex);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO_color);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+        // Vertex Color Attribute
+        glBindBuffer(GL_ARRAY_BUFFER, VBO_color);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(1);
+
+
+        unsigned int VBO, VAO;
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+        glBindVertexArray(VAO);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+
+        // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+        glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+        // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+        // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+        glBindVertexArray(0);
+    }
 
     glEnable(GL_DEPTH_TEST);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glDepthFunc(GL_LESS); // The default behavior, fragments with smaller depth values pass.
     glfwWindowHint(GLFW_DEPTH_BITS, 24); // 24-bit depth buffer (adjust to your needs)
-
-    unsigned int VBO_vertex, VBO_color;
-    glGenBuffers(1, &VBO_vertex);
-    glGenBuffers(1, &VBO_color);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO_vertex);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-        // Vertex Position Attribute
-    glBindBuffer(GL_ARRAY_BUFFER, VBO_vertex);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO_color);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
-    // Vertex Color Attribute
-    glBindBuffer(GL_ARRAY_BUFFER, VBO_color);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(1);
-
-    std::vector<GLuint> VAOs;
-    std::vector<GLuint> VBOs;
 
     // render loop
     // -----------
@@ -386,161 +411,6 @@ void hw_3_3(const std::vector<std::string> &params) {
 
         Matrix4x4f world_to_screen =
             camera_to_screen * projection * inverse(scene.camera.cam_to_world);
-
-        for (const auto &mesh : scene.meshes) {
-            GLuint VAO, VBO_vertex, VBO_color;
-            glGenVertexArrays(1, &VAO);
-            glBindVertexArray(VAO);
-
-            glGenBuffers(1, &VBO_vertex);
-            glGenBuffers(1, &VBO_color);
-
-            glBindBuffer(GL_ARRAY_BUFFER, VBO_vertex);
-            glBufferData(GL_ARRAY_BUFFER, mesh.vertices.size() * sizeof(float), &mesh.vertices[0], GL_STATIC_DRAW);
-
-            // Vertex Position Attribute
-            glBindBuffer(GL_ARRAY_BUFFER, VBO_vertex);
-            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-            glEnableVertexAttribArray(0);
-
-            glBindBuffer(GL_ARRAY_BUFFER, VBO_color);
-            glBufferData(GL_ARRAY_BUFFER, mesh.vertex_colors.size() * sizeof(float), &mesh.vertex_colors[0], GL_STATIC_DRAW);
-
-            // Vertex Color Attribute
-            glBindBuffer(GL_ARRAY_BUFFER, VBO_color);
-            glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-            glEnableVertexAttribArray(1);
-
-            // Store the VAO and VBOs for this mesh
-            VAOs.push_back(VAO);
-            VBOs.push_back(VBO_vertex);
-            VBOs.push_back(VBO_color);
-        }
-
-        for (int y = 0; y < img.height; y++) {
-            for (int x = 0; x < img.width; x++) {
-                Vector3 sum{0, 0, 0};
-                int n = 4;
-                for (int dx = 0; dx < n; dx++) {
-                    for (int dy = 0; dy < n; dy++) {
-                        Real xoff = (dx + Real(0.5)) / Real(n);
-                        Real yoff = (dy + Real(0.5)) / Real(n);
-                        Vector2 pixel_center{x + xoff, y + yoff};
-                        Vector3 color = Vector3{0.5, 0.5, 0.5}; // background color
-                        Real z_min = infinity<Real>();
-                        for (const auto &mesh : scene.meshes) {
-
-                            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-                            glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-
-                            // construct the MVP matrix
-                            Matrix4x4 MVP = world_to_screen * mesh.model_matrix;
-                            for (int i = 0; i < (int)mesh.faces.size(); i++) {
-                                Vector3 p0 = mesh.vertices[mesh.faces[i][0]];
-                                Vector3 p1 = mesh.vertices[mesh.faces[i][1]];
-                                Vector3 p2 = mesh.vertices[mesh.faces[i][2]];
-                                Vector3 screen_p0 = xform_point(MVP, p0);
-                                Vector3 screen_p1 = xform_point(MVP, p1);
-                                Vector3 screen_p2 = xform_point(MVP, p2);
-
-                                // We don't implement clipping here, if even one of the 
-                                // vertex is behind the clipping plane, we skip the triangle
-                                // screen_pi.z stores 1/pi.z
-                                Real inv_z_near = (1/scene.camera.z_near);
-                                if (screen_p0.z > inv_z_near ||
-                                        screen_p1.z > inv_z_near ||
-                                        screen_p2.z > inv_z_near) {
-                                    continue;
-                                }
-
-                                Vector2 sp0 = Vector2{screen_p0.x, screen_p0.y};
-                                Vector2 sp1 = Vector2{screen_p1.x, screen_p1.y};
-                                Vector2 sp2 = Vector2{screen_p2.x, screen_p2.y};
-
-                                Vector2 e01 = sp1 - sp0, e12 = sp2 - sp1, e20 = sp0 - sp2;
-                                auto normal = [](const Vector2 &v) {
-                                    return Vector2{v.y, -v.x};
-                                };
-                                Vector2 n01 = normal(e01),
-                                        n12 = normal(e12),
-                                        n20 = normal(e20);
-
-                                bool s01 = dot(pixel_center - sp0, n01) > 0;
-                                bool s12 = dot(pixel_center - sp1, n12) > 0;
-                                bool s20 = dot(pixel_center - sp2, n20) > 0;
-                                if ((s01 && s12 && s20) || (!s01 && !s12 && !s20)) {
-                                    // Inside the triangle
-                                    // Interpolate Z
-                                    
-                                    // 1. convert pixel center from screen space to camera space
-                                    auto to_camera = [&](const Vector2 &p) {
-                                        // camera to screen:
-                                        // x'' = w ((x' + sa)/ (2sa))
-                                        // y'' = -h ((y' - s)/ (2s))
-                                        // screen to camera:
-                                        // x' = 2sa x'' / w - sa
-                                        // y' = - 2s y'' / h + s
-                                        return Vector2{(2 * s * a) * p.x / img.width - s * a,
-                                                    - 2 * s * p.y / img.height + s};
-                                    };
-                                    Vector2 pp = to_camera(pixel_center);
-                                    Vector2 pp0 = to_camera(sp0);
-                                    Vector2 pp1 = to_camera(sp1);
-                                    Vector2 pp2 = to_camera(sp2);
-
-                                    // 2. compute projected barycentric coordinates
-                                    auto area = [&](const Vector2 pp0,
-                                                    const Vector2 pp1,
-                                                    const Vector2 pp2) {
-                                        // lift things to 3D to take cross product
-                                        Vector3 p0 = Vector3{pp0.x, pp0.y, Real(0)};
-                                        Vector3 p1 = Vector3{pp1.x, pp1.y, Real(0)};
-                                        Vector3 p2 = Vector3{pp2.x, pp2.y, Real(0)};
-                                        return length(cross(p1 - p0, p2 - p0)) / 2;
-                                    };
-
-                                    Real a0 = area(pp, pp1, pp2);
-                                    Real a1 = area(pp0, pp, pp2);
-                                    Real a2 = area(pp0, pp1, pp);
-                                    Real denom = area(pp0, pp1, pp2);
-                                    Real b0p = a0 / denom;
-                                    Real b1p = a1 / denom;
-                                    Real b2p = a2 / denom;
-
-                                    // 3. compute the original barycentric coordinates
-                                    b0p *= screen_p0.z;
-                                    b1p *= screen_p1.z;
-                                    b2p *= screen_p2.z;
-                                    Real b0 = b0p / (b0p + b1p + b2p);
-                                    Real b1 = b1p / (b0p + b1p + b2p);
-                                    Real b2 = b2p / (b0p + b1p + b2p);
-
-                                    // 4. interpolate and obtain Z
-                                    Real z = b0 / screen_p0.z +
-                                            b1 / screen_p1.z +
-                                            b2 / screen_p2.z;
-                                    // Remember to negate to make Z positive
-                                    z = fabs(z);
-
-                                    // Now, test if Z is smaller
-                                    if (z < z_min) {
-                                        // Interpolate vertex colors
-                                        Vector3 C0 = mesh.vertex_colors[mesh.faces[i][0]];
-                                        Vector3 C1 = mesh.vertex_colors[mesh.faces[i][1]];
-                                        Vector3 C2 = mesh.vertex_colors[mesh.faces[i][2]];
-
-                                        color = b0 * C0 + b1 * C1 + b2 * C2;
-                                        z_min = z;
-                                    }
-                                }
-                            }
-                        }
-                        sum += color;
-                    }
-                }
-                img(x, y) = sum / Real(n * n);
-            }
-        }
         
         
         // input
