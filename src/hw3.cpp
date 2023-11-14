f125886cba2863f1467c1d5a9d89e12e99ad4c2d
hsdehaini <99140276+hsdehaini@users.noreply.github.com>
2023-11-14 01:02:10 -0800

    Working on 3.3

--- src/hw3.cpp
+++ src/hw3.cpp
@@ -318,55 +318,24 @@ void hw_3_3(const std::vector<std::string> &params) {
 
     for(int i = 0; i < scene.meshes.size(); i++) {
         glGenBuffers(1, &VBO_vertex[i]);
-        glGenBuffers(1, &VBO_color[i]);
-        glGenBuffers(1, &EBOs[i]);
-        glGenVertexArrays(1, &VAOs[i]);
-
         glBindBuffer(GL_ARRAY_BUFFER, VBO_vertex[i]);
         glBufferData(GL_ARRAY_BUFFER, sizeof(scene.meshes.ver), vertices, GL_STATIC_DRAW);
-        // Vertex Position Attribute
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
         glEnableVertexAttribArray(0);
 
+
+        glGenBuffers(1, &VBO_color[i]);
         glBindBuffer(GL_ARRAY_BUFFER, VBO_color[i]);
         glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
-        // Vertex Color Attribute
         glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
         glEnableVertexAttribArray(1);
 
+        glGenBuffers(1, &EBOs[i]);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOs[i]);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-
+        glGenVertexArrays(1, &VAOs[i]);
         glBindVertexArray(VAOs[i]);
-
-
-        glBindBuffer(GL_ARRAY_BUFFER, VBO_vertex);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-            // Vertex Position Attribute
-        glBindBuffer(GL_ARRAY_BUFFER, VBO_vertex);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-        glEnableVertexAttribArray(0);
-
-        glBindBuffer(GL_ARRAY_BUFFER, VBO_color);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
-        // Vertex Color Attribute
-        glBindBuffer(GL_ARRAY_BUFFER, VBO_color);
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-        glEnableVertexAttribArray(1);
-
-
-        unsigned int VBO, VAO;
-        glGenVertexArrays(1, &VAO);
-        glGenBuffers(1, &VBO);
-        // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-        glBindVertexArray(VAO);
-
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-        glEnableVertexAttribArray(0);
 
         // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
         glBindBuffer(GL_ARRAY_BUFFER, 0); 
