aa692d6662a554366b1a3dbd4e8fd58708feaff7
hsdehaini <99140276+hsdehaini@users.noreply.github.com>
2023-11-17 00:13:37 -0800

    3.3 done

--- src/hw3.cpp
+++ src/hw3.cpp
@@ -3,8 +3,32 @@
 #include "3rdparty/glfw/include/GLFW/glfw3.h"
 #include "hw3_scenes.h"
 #include "image.h"
+#include "vector.h"
+#include "glm/glm/glm.hpp"
+#include "glm/glm/gtc/matrix_transform.hpp"
+#include "glm/glm/gtc/type_ptr.hpp"
+#include "shader.h"
 
 using namespace hw3;
+
+// camera
+glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f, 0.0f);
+
+bool firstMouse = true;
+float yaw   = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+float pitch =  0.0f;
+float lastX =  800.0f / 2.0;
+float lastY =  600.0 / 2.0;
+float fov   =  45.0f;
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+
+// lighting
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 
 void hw_3_1(const std::vector<std::string> &params) {
     glfwInit();
@@ -43,6 +67,19 @@ void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    float cameraSpeed = static_cast<float>(2.5 * deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
@@ -207,6 +244,56 @@ void hw_3_2(const std::vector<std::string> &params) {
 
 
 
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
+
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+    lastX = xpos;
+    lastY = ypos;
+
+    float sensitivity = 0.1f; // change this value to your liking
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    // make sure that when pitch is out of bounds, screen doesn't get flipped
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+    glm::vec3 front;
+    front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    front.y = sin(glm::radians(pitch));
+    front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(front);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    fov -= (float)yoffset;
+    if (fov < 1.0f)
+        fov = 1.0f;
+    if (fov > 45.0f)
+        fov = 45.0f;
+}
+
 void hw_3_3(const std::vector<std::string> &params) {
     // HW 3.3: Render a scene
     if (params.size() == 0) {
@@ -261,6 +348,11 @@ void hw_3_3(const std::vector<std::string> &params) {
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    // tell GLFW to capture our mouse
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     // glad: load all OpenGL function pointers
     // ---------------------------------------
@@ -315,6 +407,11 @@ void hw_3_3(const std::vector<std::string> &params) {
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window)) {
+        // per-frame time logic
+        // --------------------
+        float currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
         // input
         // -----
         processInput(window);
@@ -432,7 +529,7 @@ void hw_3_3(const std::vector<std::string> &params) {
 
             // Set uniforms
             glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "projection"), 1, GL_FALSE, projection.ptr());
-            glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "view"), 1, GL_FALSE, inverse(scene.camera.cam_to_world).ptr());
+            glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp)));
             glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, scene.meshes[i].model_matrix.ptr());
 
             // Bind VAO
@@ -477,4 +574,266 @@ void hw_3_4(const std::vector<std::string> &params) {
 
     Scene scene = parse_scene(params[0]);
     std::cout << scene << std::endl;
+
+    const unsigned int SCR_WIDTH = scene.camera.resolution.x;
+    const unsigned int SCR_HEIGHT = scene.camera.resolution.y;
+
+    const char *vertexShaderSource ="#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "layout (location = 1) in vec3 aColor;\n"
+        "out vec3 ourColor;\n"
+        "uniform mat4 model;\n"
+        "uniform mat4 view;\n"
+        "uniform mat4 projection;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = projection * view * model * vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "   ourColor = aColor;\n"
+        "}\0";
+
+    const char *fragmentShaderSource = "#version 330 core\n"
+        "in vec3 ourColor;\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "   FragColor = vec4(ourColor, 1.0f);\n"
+        "}\n\0";
+        
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Balboa", NULL, NULL);
+    std::cout << "SCR_WIDTH: " << SCR_WIDTH << std::endl;
+    std::cout << "SCR_HEIGHT: " << SCR_HEIGHT << std::endl;
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    // tell GLFW to capture our mouse
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+    }
+
+
+    // build and compile our shader program
+    // ------------------------------------
+    // vertex shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // fragment shader
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // link shaders
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    GLuint VAOs;
+    GLuint VBO_vertex;
+    GLuint VBO_color;
+    GLuint EBOs;
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window)) {
+        // per-frame time logic
+        // --------------------
+        float currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+        // input
+        // -----
+        processInput(window);
+        Image3 img(SCR_WIDTH, SCR_HEIGHT);
+        // Construct perspective projection matrix
+        Real w = Real(scene.camera.resolution.x);
+        Real h = Real(scene.camera.resolution.y);
+        Real s = Real(scene.camera.s);
+        Real a = Real(w) / Real(h);
+        Real z_near = Real(scene.camera.z_near);
+        Real z_far = Real(scene.camera.z_far);
+
+        Matrix4x4f projection = Matrix4x4f(
+            1.0 / (a * s), 0.0, 0.0, 0.0,
+            0.0, 1.0 / s, 0.0, 0.0,
+            0.0, 0.0, -z_far / (z_far - z_near), (-z_far * z_near) / (z_far - z_near),
+            0.0, 0.0, -1.0, 0.0
+        );
+
+        // std::cout << "projection: " << projection << std::endl;
+
+        // render
+        // ------
+        glClearColor(Vector3f (scene.background).x, Vector3f(scene.background).y, Vector3f(scene.background).z, 1.0f);
+        glEnable(GL_DEPTH_TEST);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glEnable(GL_FRAMEBUFFER_SRGB); 
+
+        for (std::size_t i = 0; i < scene.meshes.size(); ++i) {
+            auto mesh = scene.meshes[i];
+
+            std::vector<float> vertex_array = {};
+            std::vector<float> color_array = {};
+            std::vector<unsigned int> face_array = {};
+
+            for(int j = 0; j < mesh.vertices.size(); ++j) {
+
+                auto vertex = Vector3f(mesh.vertices[j]);
+
+                vertex_array.push_back(vertex.x);
+                vertex_array.push_back(vertex.y);
+                vertex_array.push_back(vertex.z);
+            }
+            
+            float vertices_size [vertex_array.size()];
+            for(int j = 0; j < vertex_array.size(); ++j) {
+                vertices_size[j] = vertex_array[j];
+            }
+
+
+
+            for(int j = 0; j < mesh.vertex_colors.size(); ++j) {
+
+                auto color = Vector3f(mesh.vertex_colors[j]);
+
+                color_array.push_back(color.x);
+                color_array.push_back(color.y);
+                color_array.push_back(color.z);
+            }
+            
+            float colors_size [color_array.size()];
+            for(int j = 0; j < color_array.size(); ++j) {
+                colors_size[j] = color_array[j];
+            }
+
+            
+
+            for(int j = 0; j < mesh.faces.size(); ++j) {
+
+                auto face = Vector3i(mesh.faces[j]);
+
+                face_array.push_back(face.x);
+                face_array.push_back(face.y);
+                face_array.push_back(face.z);
+            }
+            
+            unsigned int face_size [face_array.size()];
+            for(int j = 0; j < face_array.size(); ++j) {
+                face_size[j] = face_array[j];
+            }
+
+            
+        
+            glGenVertexArrays(1, &VAOs);
+            glBindVertexArray(VAOs);
+
+            // Create VBO for vertices
+            glGenBuffers(1, &VBO_vertex);
+            glBindBuffer(GL_ARRAY_BUFFER, VBO_vertex);
+            glBufferData(GL_ARRAY_BUFFER, vertex_array.size() * sizeof(float), vertices_size, GL_STATIC_DRAW);
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+            glEnableVertexAttribArray(0);
+
+            // Create VBO for colors
+            glGenBuffers(1, &VBO_color);
+            glBindBuffer(GL_ARRAY_BUFFER, VBO_color);
+            glBufferData(GL_ARRAY_BUFFER, color_array.size() * sizeof(float), colors_size, GL_STATIC_DRAW);
+            glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+            glEnableVertexAttribArray(1);
+
+            glGenBuffers(1, &EBOs);
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOs);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, face_array.size() * sizeof(unsigned int), face_size, GL_STATIC_DRAW);
+
+            // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+            glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+            // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+            // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+            glBindVertexArray(0);
+
+            // Set shader program
+            glUseProgram(shaderProgram);
+
+            // Set uniforms
+            glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "projection"), 1, GL_FALSE, projection.ptr());
+            glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp)));
+            glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, scene.meshes[i].model_matrix.ptr());
+
+            // Bind VAO
+            glBindVertexArray(VAOs);
+
+            // Draw the mesh using indexed rendering
+            glDrawElements(GL_TRIANGLES, mesh.faces.size() * 3, GL_UNSIGNED_INT, 0);
+
+            // i++;
+            glBindVertexArray(0);
+        }
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+            // -------------------------------------------------------------------------------
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+    }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    // Delete VAOs
+    glDeleteVertexArrays(1, &VAOs);
+
+    // Delete VBOs
+    glDeleteBuffers(1, &VBO_vertex);
+    glDeleteBuffers(1, &VBO_color);
+    glDeleteBuffers(1, &EBOs);
+
+    // Delete shader program
+    glDeleteProgram(shaderProgram);
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
 }
